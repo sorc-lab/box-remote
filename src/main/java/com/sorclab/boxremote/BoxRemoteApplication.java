9128f9fe62d2f707908543513a5f6c62ac845200
macfisher <macfisher01@gmail.com>
2020-05-11 07:09:48 -0600

    Initial commit

---
+++ src/main/java/com/sorclab/boxremote/BoxRemoteApplication.java
@@ -0,0 +1,13 @@
+package com.sorclab.boxremote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxRemoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxRemoteApplication.class, args);
+	}
+
+}
