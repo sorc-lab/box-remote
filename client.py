0f8844a501412aab888e3f479b96ddc91a783cd1
macfisher <macfisher01@gmail.com>
2020-05-12 06:40:40 -0600

    RDBX-1: Client calls correct endpoint

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 
 import requests
 
-resp = requests.get('http://localhost:8080/bleh')
+resp = requests.get('http://localhost:8080/')
 
 if (resp.status_code != 200):
     # https://stackoverflow.com/questions/30970905/python-conditional-exception-messages
