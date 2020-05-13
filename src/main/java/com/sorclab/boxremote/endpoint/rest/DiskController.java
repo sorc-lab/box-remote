fd033d51fe025d80974f6ca99e0bcd3832961115
macfisher <macfisher01@gmail.com>
2020-05-12 22:11:11 -0600

    RDBX-9: getDiskIO and started on dir size/file tree endpoint

--- src/main/java/com/sorclab/boxremote/endpoint/rest/DiskController.java
+++ src/main/java/com/sorclab/boxremote/endpoint/rest/DiskController.java
@@ -8,16 +8,41 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.List;
 
+// TODO: Logs are completely silent. 500 w/ "Command failed" prints 0 stack traces--proper status is returned to client
+
 @RestController
 public class DiskController
 {
     @GetMapping(value = "/disk/space")
-    public List<String> diskSpace()
+    public List<String> getDiskSpace()
     {
         try {
             return LinuxUtils.shellExec(new String[]{"df", "-h"});
         } catch (IOException e) {
             throw new InternalServerError("Command failed", e);
+        }
+    }
+
+    @GetMapping(value = "/disk/io")
+    public List<String> getDiskIO()
+    {
+        try {
+            //return LinuxUtils.shellExec(new String[]{"top", "-n 1", "-b"});
+            return LinuxUtils.shellExec(new String[]{"iostat"});
+        } catch (IOException e) {
+            throw new InternalServerError("Command failed", e);
+        }
+    }
+
+    @GetMapping(value = "/disk/dirsize")
+    public List<String> getDirSize()
+    {
+        try {
+            return LinuxUtils.shellExec(new String[]{"cd", "/var", "&&", "sudo", "du", "-shc", "."});
+            //return LinuxUtils.shellExec(new String[]{"ls"});
+        } catch (IOException e) {
+            e.printStackTrace(); // TODO: Remove after fix to below issue
+            throw new InternalServerError("Command failed", e); // TODO: Why is this swallowing the stack trace?
         }
     }
 }
